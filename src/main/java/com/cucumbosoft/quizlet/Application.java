aea689b20ed059c077919357860161aee8f40757
gonzalez-j <gonzalez-j@users.noreply.github.com>
2016-10-05 19:55:11 -0400

    Add files via upload
    0.1 upload

---
+++ src/main/java/com/cucumbosoft/quizlet/Application.java
@@ -0,0 +1,7 @@
+package com.cucumbosoft.quizlet;
+
+class Application {
+    public static void main(String[] args) {
+        new QuizletDownloader().downloader();
+    }
+}
---
+++ src/main/java/com/cucumbosoft/quizlet/QuizletDownloader.java
@@ -0,0 +1,67 @@
+package com.cucumbosoft.quizlet;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created on 10/5/2016
+ */
+class QuizletDownloader {
+    private final HttpClient connection = HttpClientBuilder.create().build();
+    private final List<String> words    = new ArrayList<>();
+    private Document page;
+
+    public void downloader() {
+        String pageUrl   = JOptionPane.showInputDialog("enter quizlet url", JOptionPane.PLAIN_MESSAGE);
+        Object separator = JOptionPane.showInputDialog(null,
+                "select separator", "input",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                new String[] {": ", " - "}, ": "
+        );
+
+        try {
+            page = Jsoup.parse(EntityUtils.toString(connection.execute(new HttpGet(pageUrl)).getEntity()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        words.addAll(
+                page.select(".terms > *").stream()
+                    .map(card ->
+                        card.attr("class", "text")
+                            .attr("class", "word has-audio ")
+                            .getElementsByTag("h3")
+                            .first()
+                            .text() +
+                        separator +
+                        card.attr("class", "text")
+                            .attr("class", "definition has-audio ")
+                            .getElementsByTag("span")
+                            .last()
+                            .text() +
+                        "\n"
+                    )
+                .sorted()
+                .collect(Collectors.toList())
+        );
+
+        // set clipboard
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(String.join("", words)), null);
+
+        // alert user to completion
+        JOptionPane.showMessageDialog(null, "Copied " + words.size() + " words.", "Complete", JOptionPane.PLAIN_MESSAGE);
+    }
+}
